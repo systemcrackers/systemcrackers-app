d44a555234382f13a418f47c75f889526cea79a2
Faizan Choudhary <faizan2000461@st.jmi.ac.in>
2022-11-17 01:15:08 +0530

    initialized project

---
+++ README.md
@@ -0,0 +1,16 @@
+# uia_app
+
+Application built for UIA 2022 Team- UIA320 PS- EDUC34
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // theme: ,
+      // routes: ,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'My App',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontFamily: 'Quicksand',
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
